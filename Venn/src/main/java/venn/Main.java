eec1a08842fd13eb2298a242ad99bf6357be21a0
ruttbez <ruttkas@my.yorku.ca>
2020-01-13 12:36:19 -0500

    demo

--- Venn/src/main/java/venn/Main.java
+++ Venn/src/main/java/venn/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("This is Tianpei_v9");
+		System.out.print("This is Tianpe i_v9");
 
 		System.out.println("Wowow");
 		System.out.println("Test");
