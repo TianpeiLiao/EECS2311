18745f89c2e55246e9b865b9021e45226aa40e1e
ArdaTemel <temela@my.yorku.ca>
2020-01-13 12:19:30 -0500

    First Push Arda

--- Venn/src/main/java/venn/Main.java
+++ Venn/src/main/java/venn/Main.java
@@ -7,6 +7,7 @@ public class Main {
 		System.out.println("Wowow");
 		System.out.println("Test");
 		System.out.println("Test 2");
+		System.out.println("Is this working --Arda");
 		System.out.println("Test 2");
 		System.out.println("Test 2");
 		System.out.println("Test 2");
