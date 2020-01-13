3e21b1dee8935d7e0c2fa3e4e1c0586a1aa4859f
ArdaTemel <temela@my.yorku.ca>
2020-01-13 12:38:24 -0500

    some change

--- Venn/src/main/java/venn/Main.java
+++ Venn/src/main/java/venn/Main.java
@@ -5,11 +5,11 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("This is Tianpe i_v9");
-
+		
 		System.out.println("Wowow!!!");
 		System.out.println("Test");
 		System.out.println("Test 2");
-		System.out.println("Is this working --Arda");
+		System.out.println("Is this working --Arda temel");
 		System.out.println("Test 2");
 		System.out.println("Test 2");
 		System.out.println("Test 2");
