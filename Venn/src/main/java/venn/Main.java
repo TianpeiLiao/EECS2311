2484e1d9af35e957b580f35d4210148122ec0056
TianpeiLiao <liaot3@my.yorku.ca>
2020-01-13 12:37:09 -0500

    !!!

--- Venn/src/main/java/venn/Main.java
+++ Venn/src/main/java/venn/Main.java
@@ -6,7 +6,7 @@ public class Main {
 		// TODO Auto-generated method stub
 		System.out.print("This is Tianpe i_v9");
 
-		System.out.println("Wowow");
+		System.out.println("Wowow!!!");
 		System.out.println("Test");
 		System.out.println("Test 2");
 		System.out.println("Is this working --Arda");
