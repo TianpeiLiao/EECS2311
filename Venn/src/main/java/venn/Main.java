85f2f0a3268b8b63ee143a66e10007c713da58d0
ArdaTemel <temela@my.yorku.ca>
2020-01-20 15:57:37 -0500

    Commit

--- Venn/src/main/java/venn/Main.java
+++ Venn/src/main/java/venn/Main.java
@@ -91,30 +91,6 @@ public class Main extends Application{
 		primaryStage.setScene(s);
 		primaryStage.show();
 	
-
-
-
-
-
-
-public class Main{
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-//		System.out.print("This is Tianpe i_v9");
-//		System.out.println("Wowow!!!");
-//		System.out.println("Test");
-//		System.out.println("Test 2");
-//		System.out.println("Is this working --Arda temel");
-//		System.out.println("Test 2");
-//		System.out.println("Test 2");
-//		System.out.println("Test 2");
-//		System.out.println("final");
-//		System.out.println("new ");
-//		
-		
 	}
-
-	
 
 }
