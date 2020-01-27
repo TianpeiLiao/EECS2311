32618439a3afc533e8540f4ecf7529003d3cef0e
ArdaTemel <temela@my.yorku.ca>
2020-01-27 11:55:01 -0500

    Changes some UI elements

--- Venn/src/main/java/venn/VennController.java
+++ Venn/src/main/java/venn/VennController.java
@@ -1,26 +1,33 @@
 package venn;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ColorPicker;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+
 
 public class VennController {
 
 	@FXML
+	public static FlowPane txtField;
+	@FXML
 	private Button create;
+	@FXML
+	private Button newScene;
 	@FXML
 	private Button dltEntry;
 	@FXML
@@ -30,9 +37,8 @@ public class VennController {
 	@FXML
 	private ColorPicker cp;
 	@FXML
-	private FlowPane txtField;
-	@FXML
 	private Text mytxt;
+	
 	
 	private ArrayList<DraggableText> entries = new ArrayList<DraggableText>();
 	int entrycount = 0;
@@ -41,12 +47,28 @@ public class VennController {
 		cp.getStyleClass().add("split-button");
 	}
 	
+	public void openNewScene(ActionEvent e) {
+		try {
+			Parent fxml  = FXMLLoader.load(getClass().getResource("getData.fxml"));
+			Stage secondStage = new Stage();
+			secondStage.setTitle("Enter Data");
+			secondStage.setScene(new Scene(fxml));
+			secondStage.show();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		
+	}
+	
+	
 	public void createText(ActionEvent e) {
 		if(!entryName.getText().isEmpty() ) {
 		 Color c = cp.getValue();	
 		 double radi  = 10;
 		 DraggableText newTxt = new DraggableText(entryName.getText(), c, radi);
-		 newTxt.setFont(Font.font("Roboto Slab", FontWeight.NORMAL, 10));
+		 newTxt.setFont(Font.font("Roboto Slab", FontWeight.NORMAL, 15));
 		 newTxt.getStyleClass().add("createdText");
 		 entries.add(newTxt);
 		 txtField.getChildren().add(entries.get(entrycount));
